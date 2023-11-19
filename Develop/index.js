8854a2e57ab950cde5dd618c344c24e35c08da45
devinshade <devindanielle93@gmail.com>
2023-11-18 17:21:49 -0700

    installed inquirer and answered TODOs

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,87 @@
-// TODO: Include packages needed for this application
+// console.log("hellooo, node")
+// ! TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// ! TODO: Create an array of questions for user input
+const questions = [
+    'What is the title of your project?',
+    'What is the description of your project?',
+    'What are the installation instructions?',
+    'What is the usage information?',
+// Choose a license from a list of options
+    'Choose a license for your project:',
+    'What are the contribution guidelines?',
+    'What are the test instructions?',
+// GitHub Username and Email address are added to the Questions section
+    // with instructions on how to reach you with additional questions
+    'What is your GitHub username?',
+    'What is your email address?'
+];
+// console.log(questions)
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is your name?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Where are you from?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What is your favorite hobby?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What is your favorite food?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Enter your LinkedIn URL.',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter your LinkedIn URL.',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your LinkedIn URL.',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your LinkedIn URL.',
+      },
+    ]);
+  };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(`File ${fileName} has been saved.`);
+      });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,104 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// ! TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'GNU GPLv3':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
+// ! TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GNU GPLv3':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    // Add more cases for other licenses if needed
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// ! TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case 'MIT':
+      return `
+## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+`;
+    case 'Apache 2.0':
+      return `
+## License
+
+This project is licensed under the [Apache License 2.0](https://opensource.org/licenses/Apache-2.0).
+`;
+    case 'GNU GPLv3':
+      return `
+## License
+
+This project is licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0).
+`;
+    default:
+      return '';
+  }
+}
+
+// ! TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
 
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  ${data.license}
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Questions
+  
+  For any questions, please contact me:
+  
+  GitHub: [${data.github}](https://github.com/${data.github})
+  
+  Email: ${data.email}
 `;
 }
 
